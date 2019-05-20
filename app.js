5ebce529656ce9e617b395b8aa2f9246dc0a2f61
daddyishoh <50054033+daddyishoh@users.noreply.github.com>
2019-05-20 02:36:36 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+var text = [ "I love you so much baby", "I wanted to tell you how much you mean to me....", "how thankful I am....", "that you have stuck by me and...", 
+    "believed in me....", "when I didn't even believe in myself...", "but.....", "I can't find the words....", "none seem to fit quite right...",
+    "so I'll have to leave it at....", "HAPPY BIRTHDAY!!! I LOVE YOU!"];
+var counter = 0;
+var elem = document.getElementById("changeText");
+var inst = setInterval(change, 2000);
+
+function change() {
+  elem.innerHTML = text[counter];
+  counter++;
+  if (counter >= text.length) {
+    counter = 0;
+    clearInterval(inst); // uncomment this if you want to stop refreshing after one cycle
+  }
+}
